6aac2f0c948a4111cecb16271955c7632e92c4d8
Belle <bellepx3@gmail.com>
2019-10-02 09:33:51 -0400

    worked on events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,7 @@ class EventsController < ApplicationController
     end
 
     def show
+        @event = Event.find(params[:id])
         redirect_to event_path(@event)
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,21 @@ Location.destroy_all
 u1 = User.create(first_name: "Karina", last_name: "Macancela", username: "kmac", password: "kmac")
 u2 = User.create(first_name: "Kim", last_name: "Jones", username: "kjones", password: "kjones")
 u3 = User.create(first_name: "James", last_name: "Kim", username: "james", password: "james")
-
+u4 = User.create(first_name: "Belle", last_name: "P", username: "belle", password: "belle")
 
 l1 = Location.create(city: "New York", state: "New York", country: "USA", zipcode: 10011)
 l2 = Location.create(city: "Miami", state: "Florida", country: "USA", zipcode: 32963)
+l3 = Location.create(city: "Bangkok", state: nil , country: "Thailand", zipcode: 10110)
+l4 = Location.create(city: "Paris", state: nil, country: "France", zipcode: 75007)
 
 cat1 = Category.create(name: "Music")
 cat2 = Category.create(name: "Art")
 cat3 = Category.create(name: "Sports")
 cat4 = Category.create(name: "Nightlife")
 
-e1 = Event.create(user_id: 1, location_id: 1, title: "Some title of event", start_time: "10:00AM", end_time: "1:00PM", event_date: "10-10-2019")
+e1 = Event.create(user_id: u4.id, location_id: l2.id, title: "South Beach Bonfire", start_time: "7:00PM", end_time: "11:00PM", event_date: "10-10-2019")
+e2 = Event.create(user_id: u1.id, location_id: l1.id, title: "Chelsea Art Gallery Opening Reception", start_time: "6:00PM", end_time: "9:00PM", event_date: "10-10-2019")
+e3 = Event.create(user_id: u2.id, location_id: l4.id, title: "Wine Festival", start_time: "12:00PM", end_time: "6:00PM", event_date: "10-10-2019")
 
 ec1 = EventCategory.create(event_id: 1, category_id: 1)
 
